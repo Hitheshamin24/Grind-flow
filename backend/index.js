8fe1fdfa3b8054980ebf3a49a3df882dbdcf429c
Hithesh <hitheshamin423@gmail.com>
2026-02-19 20:33:02 +0530

    initial setup

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+const express=require('express')
+require('dotenv').config()
+const connect=require("./config/config")
+connect()
+const app= express()
+
+
+
+app.get("/",(req,res)=>{
+    res.send("Hello World")
+})
+app.listen(5000,()=>{
+console.log("Server is running on port "+5000)
+})
