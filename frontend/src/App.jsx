a8dd2f7b73679dc65c2387f6eaf0c031f5c0bde7
Hithesh <hitheshamin423@gmail.com>
2026-02-26 22:07:17 +0530

    add react router dom and dashboard page link

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,32 @@
-import React from 'react'
-import AuthPage from './pages/AuthPage';
-
+import React from "react";
+import AuthPage from "./pages/AuthPage";
+import { Routes, Route, Navigate, useLocation } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+const RequireAuth = ({ children }) => {
+  const token = localStorage.getItem("token");
+  const location = useLocation();
+  if (!token) {
+    return <Navigate to="/auth" replace state={{ from: location }} />;
+  }
+  return children;
+};
 const App = () => {
   return (
     <div>
-      <AuthPage />
-    </div>
-  )
-}
+      <Routes>
+        <Route path="/auth" element={<AuthPage/>}/>
+        <Route path="/dashboard" element={
+          <RequireAuth>
+            <HomePage/>
+          </RequireAuth>
+        }/>
 
-export default App
+        <Route path="" element={<Navigate to="/dashboard/"  replace/> } />
+      </Routes>
+
+
+    </div>
+  );
+};
+
+export default App;
